1621c1699837a7f0307496724184345511c0f9d8
cflorette <fchamga@yahoo.com>
2015-05-14 00:04:49 +0200

    Layout neu gemacht

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var app = angular.module("app", []);
+
+app.controller("headerCtrl", function($scope){
+    //controll for header part    
+});
+
+app.controller("footerCtrl", function($scope){
+    //.controll for footer..    
+});
+
+app.controller("menuCtrl", function($scope){
+    //...controll for menu    
+});
+
+app.controller("contentCtrl", function($scope){
+    //control for menu   
+});
